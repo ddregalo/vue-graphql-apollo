769d93c61ce5640efb02fd5f93ee4d3891023f0f
danielcampos <ddregalo@gmail.com>
2019-04-15 00:06:16 +0100

    Create graphql schema types and queries

--- apollo-graphql-server/data/schema.js
+++ apollo-graphql-server/data/schema.js
@@ -3,7 +3,36 @@ import mocks from './mocks';
 
 const typeDefs = `
 type Query {
-  testString: String
+  character(name: String, birthYear: String, gender: String, homeworld: String, films: [Film]): Author
+  allCharacters: [Character]
+  getFortuneCookie: String
+}
+
+type Character {
+  name: String
+	birthYear: String
+	gender: String
+	homeworld: Planet
+	films: [Film]
+}
+
+type Planet {
+    name: String
+    orbital_period: Integer
+    diameter: Integer
+    climate: String
+    terrain: Stirng
+    population: Integer
+    residents: [Character]
+}
+
+type Film {
+  title: String
+	episode_id: Integer
+  director: String
+	release_date: Date
+	characters: [Character]
+	planets: [Planet]
 }
 `;
 
