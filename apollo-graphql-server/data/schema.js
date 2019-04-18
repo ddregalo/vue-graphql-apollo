e311f1a6a2af269bbcf5bbcde9c715023d80f5cb
danielcampos <ddregalo@gmail.com>
2019-04-18 23:36:14 +0100

    Add props to character schema + format code

--- apollo-graphql-server/data/schema.js
+++ apollo-graphql-server/data/schema.js
@@ -1,43 +1,59 @@
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
-import mocks from './mocks';
-
+import resolvers from './resolvers';
+// import mocks from './mocks';
+// homeworld: Planet
+// films: [Film]
 const typeDefs = `
 type Query {
-  character(name: String): Character
-  allCharacters: [Character]
+  allCharacters: [Character!]!
+  character(id: Int!): Character
   getFortuneCookie: String
 }
 
 type Character {
   name: String
+  height: String
+  mass: String
+  hairColour: String
+  skinColor: Stirng
+  eyeColor: String
 	birthYear: String
-	gender: String
-	homeworld: Planet
-	films: [Film]
-}
-
-type Planet {
-    name: String
-    orbital_period: Int
-    diameter: Int
-    climate: String
-    terrain: String
-    population: Int
-    residents: [Character]
-}
-
-type Film {
-  title: String
-	episode_id: Int
-  director: String
-	release_date: String
-	characters: [Character]
-	planets: [Planet]
+  gender: String
+  homeworld: String
+  films: [String]
+  species: [String]
+  vehicles: [String]
+  starships: [String]
+  created: String
+  edited: String
+  url: Stirng
 }
 `;
 
-const schema = makeExecutableSchema({ typeDefs });
 
-addMockFunctionsToSchema({ schema, mocks });
+// type Planet {
+//   id: Int
+//   name: String
+//   orbital_period: Int
+//   diameter: Int
+//   climate: String
+//   terrain: String
+//   population: Int
+//   residents: [Character]
+// }
+
+// type Film {
+// id: Int
+// title: String
+// episode_id: Int
+// director: String
+// release_date: String
+// characters: [Character]
+// planets: [Planet]
+// }
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+// addMockFunctionsToSchema({ schema, mocks });
 
 export default schema;
