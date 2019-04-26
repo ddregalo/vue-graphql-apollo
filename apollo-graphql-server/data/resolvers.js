4e4055db57646114b3345c9783b5537e2e813980
danielcampos <ddregalo@gmail.com>
2019-04-27 00:19:41 +0100

    Add allFilms + resolve movie in character queries

--- apollo-graphql-server/data/resolvers.js
+++ apollo-graphql-server/data/resolvers.js
@@ -7,8 +7,7 @@ const resolvers = {
         allCharacters: () => {
             return axios.get(baseUrl + 'people/')
                 .then(res => {
-                    console.log('All characters response: ', res.data);
-                    return res.data
+                    return res.data.results;
                 })
                 .catch(err => {
                     console.log("There has been an error processing your request. Please try again.");
@@ -16,16 +15,39 @@ const resolvers = {
         },
         character: (parent, args) => {
             const { id } = args
-            return axios.get(baseUrl + `people/${id}`)
-                    .then(res => {
-                        console.log('Single character response: ', res.data);
-                        return res.data
-                    })
-                    .catch(error => {
-                        console.log("There has been an error processing your request. Please try again.");
-                    });
-        }
+            return axios.get(`${baseUrl}people/${id}`)
+                .then(res => {
+                    return res.data;
+                })
+                .catch(error => {
+                    console.log("There has been an error processing your request. Please try again.");
+                });
+        },
+        allFilms: () => {
+            return axios.get(baseUrl + 'films/')
+                .then(res => {
+                    return res.data.results
+                });
+        },
+        film: (parent, args) => {
+            const { id } = args
+            return axios.get(baseURL + `films/${id}}`)
+                .then(res => {
+                    res.data
+                });
+        },
+    },
+    Character: {
+        films: (parent, args, ctx, info) => {
+            let urlArray = parent.url.split('/');
+            const filmId = urlArray[urlArray.length - 2];
+            console.log('filmId: ', filmId);
+            return axios.get(`${baseUrl}films/${filmId}`)
+                .then(res => {
+                    return res.data
+                });
+        },
     }
-  };
+};
   
-  export default resolvers;
+export default resolvers;
