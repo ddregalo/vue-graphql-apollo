a449ccea24eadf5f4a5132044ac8b1dcf429564e
danielcampos <ddregalo@gmail.com>
2019-06-02 20:56:22 +0100

    Replace HelloWorld component

---
+++ vue-apollo-frontend/src/components/Characters.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="characters">
+    <div v-for="character in allCharacters" :key="character.name">
+      {{character.name}}
+    </div>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+
+export default {
+  name: "Characters",
+  apollo: {
+    allCharacters: gql`
+      query {
+        allCharacters {
+          name
+          gender
+          eye_color
+        }
+      }
+    `
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
