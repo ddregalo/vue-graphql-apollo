37b3168e905778ed7848f4599d60567b1f3be30a
danielcampos <ddregalo@gmail.com>
2019-06-02 21:47:42 +0100

    Create character component skeleton

---
+++ vue-apollo-frontend/src/components/Character.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="character">
+    </div>
+</template>
+<script lang="js">
+    export default {
+     name: "Character",
+     props: {
+      character   
+     }  
+    }
+</script>
+<style lang="scss" scoped>
+</style>
