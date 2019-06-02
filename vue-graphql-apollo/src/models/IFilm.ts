d7c35627b2d2d1a78d9c1c227dd00896cb03679e
danielcampos <ddregalo@gmail.com>
2019-06-02 11:48:41 +0100

    Replace vue frontend no typescript

--- vue-graphql-apollo/src/models/IFilm.ts
+++
@@ -1,7 +0,0 @@
-export default interface IFilm {
-    readonly title: string;
-    readonly episode_id: string;
-    readonly director: string;
-    readonly producer: string;
-    readonly opening_crawl: string;
-}
