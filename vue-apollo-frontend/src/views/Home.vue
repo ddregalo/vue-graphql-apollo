21e7b029cbc4157bd2905103cb17dad289270a97
danielcampos <ddregalo@gmail.com>
2019-06-08 00:20:10 +0100

    Add hompeage background image

--- vue-apollo-frontend/src/views/Home.vue
+++ vue-apollo-frontend/src/views/Home.vue
@@ -15,3 +15,9 @@ export default {
   }
 };
 </script>
+<style lang="scss" scoped>
+  .home {
+    background-image: url("https://media.wired.com/photos/5cd5df1f2948ca2e63b090f0/master/w_1164,c_limit/01_SPoW_051119.jpg");
+    background-color: #cccccc;
+  }
+</style>
