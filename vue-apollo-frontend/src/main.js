f14654e72e26b164c7a6b905d758d8c5270313f9
danielcampos <ddregalo@gmail.com>
2019-06-07 23:24:01 +0100

    Add Bootstrap-Vue

--- vue-apollo-frontend/src/main.js
+++ vue-apollo-frontend/src/main.js
@@ -4,7 +4,10 @@ import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 import { createProvider } from "./vue-apollo";
+import BootstrapVue from 'bootstrap-vue';
+import './css/index.scss';
 
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
