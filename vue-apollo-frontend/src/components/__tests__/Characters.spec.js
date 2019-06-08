d781367a6fd9633e1992ee154fcfa2837d6af319
danielcampos <ddregalo@gmail.com>
2019-06-08 23:42:50 +0100

    Characters component snapshot test

---
+++ vue-apollo-frontend/src/components/__tests__/Characters.spec.js
@@ -0,0 +1,23 @@
+import { shallowMount } from "@vue/test-utils";
+import Characters from "@/components/Characters.vue";
+import Vue from 'vue';
+
+Vue.config.ignoredElements = [
+  'b-card-group',
+];
+
+describe("Characters.vue", () => {
+    it("renders the correct html for single charater snapshot", () => {
+        const wrapper = shallowMount(Characters);
+        wrapper.setData({
+            allCharacters: [
+                {
+                name: 'DarkSideOfTheMoon',
+                gender: 'male',
+                eye_color: 'brown',
+                },
+            ],
+        });
+        expect(wrapper.element).toMatchSnapshot();
+    });
+});
