541f8a9ab8adccdc34bd00f22a2e8a17fdd9c3d1
danielcampos <ddregalo@gmail.com>
2019-06-08 23:57:42 +0100

    Home view end-2-end test skeleon items and links

---
+++ vue-apollo-frontend/tests/e2e/specs/HomePageView.js
@@ -0,0 +1,20 @@
+// https://docs.cypress.io/api/introduction/api.html
+
+describe("Home Page View", () => {
+  it("Visits the home page and has title", () => {
+    cy.visit("/");
+    cy.contains("h1", "StarWars Characters");
+  });
+
+  it("Visits the home page and has Home and About Link", () => {
+    cy.visit("/");
+    cy.get('[href="/"]');
+    cy.get('[href="/about"]');
+  });
+
+  it("Visits the home page and clicks to visit About page", () => {
+    cy.visit("/");
+    cy.get('[href="/about"]').click();
+    cy.contains('This is an about page');
+  });
+});
