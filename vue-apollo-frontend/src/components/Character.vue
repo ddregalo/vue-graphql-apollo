fbbd28e3cc8b246d1977c491d8736f61e7c94261
danielcampos <ddregalo@gmail.com>
2019-06-13 23:29:52 +0100

    Implement basic according in character card

--- vue-apollo-frontend/src/components/Character.vue
+++ vue-apollo-frontend/src/components/Character.vue
@@ -10,7 +10,13 @@
                 </div>
             </b-card-text>
             <div slot="footer">
-                <small class="text-muted">Data From Starwars Api 2019</small>
+                <div>
+                    <b-button block href="#" v-b-toggle="'accordion' + character.name.trim()" variant="info">Movies</b-button>
+                    <b-collapse :id="'accordion' + character.name.trim()" visible accordion="my-accordion" role="tabpanel">
+                        <b-card-text>Movies listed here</b-card-text>
+                        <b-card-text>{{ text }}</b-card-text>
+                    </b-collapse>
+                </div>
             </div>
         </b-card>
     </div>
@@ -42,6 +48,17 @@
         display: grid;
         grid-template-columns: 50% 50%;
         grid-template-rows: 50% 50%;
-        overflow-y: scroll;
+        overflow-y: scroll;  
+    }
+
+    .movies {
+        min-width: 207px;
+        background-color: rgb(17, 0, 255);
+        font-weight: 500;
+    }
+
+    #acordion-1 {
+        min-width: 190px;
+        max-width: 200px;
     }
 </style>
